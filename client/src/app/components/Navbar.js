758036b5b6e00b07169798bafb8f5bf5015fb80b
kobrak1 <burakkarhan657@gmail.com>
2024-04-04 11:53:36 +0300

    chore: boilerplate is set

---
+++ client/src/app/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-black h-18'>
+            <div>hello world</div>
+            <div>hello world</div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from './components/Navbar.js'
+import './globals.css'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
