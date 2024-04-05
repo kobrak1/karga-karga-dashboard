f128d73727a7c99a51cb315edec62229ca49576e
kobrak1 <burakkarhan657@gmail.com>
2024-04-05 18:38:09 +0300

    add: added some style on Navbar component

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-black h-18'>
-            <div className=''>kargakarga</div>
-            <div>hello world</div>
+        <nav className='flex items-center justify-between bg-gray-200 py-6 px-6'>
+            <div className='flex items-center justify-center font-italic'>kargakarga</div>
+            <div>
+                <button onClick={() => handleLogout()} >logout</button>
+            </div>
         </nav>
     )
 }
